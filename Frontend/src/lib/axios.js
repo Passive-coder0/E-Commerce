857d14f663a7ef983dcf09d0cd26c8e159266d32
Passive-coder0 <120179395+Passive-coder0@users.noreply.github.com>
2025-02-11 22:51:48 +0200

    Updated axios

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:5000/api": "/api", // The backend server URL, whether it be in development or deployed mode
+    baseURL: import.meta.mode === "development" ? "http://localhost:5000/api": "https://e-commerce-production-3024.up.railway.app//api", // The backend server URL, whether it be in development or deployed mode
     withCredentials: true, // Sends cookies to server
 });
 
